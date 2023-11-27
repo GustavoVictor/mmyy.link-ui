4c03bbc14ce7668c22434313d49c6e9ed6f99fb6
Gustavo Assuncao <gustavovictor94@gmail.com>
2023-11-26 21:34:26 -0300

    Add config para Amplify

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+      alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
+      },
+    ]
+  }
 })
